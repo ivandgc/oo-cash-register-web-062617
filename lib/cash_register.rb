a6038fd78f56a127089b3713dd6d75ee7a8f64f0
ivandgc <ivandgc@hotmail.com>
2017-06-29 09:47:06 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,36 @@
+require 'pry'
 
+class CashRegister
+
+  attr_accessor :total, :discount, :items, :transaction
+
+    def initialize(employee_discount = 0)
+      @total = 0
+      @discount = employee_discount
+      @items = []
+      @transaction = []
+    end
+
+    def add_item(item, price, quatinty = 1)
+      self.total += price * quatinty
+      quatinty.times do
+          self.items << item
+      end
+      self.transaction << price
+    end
+
+    def apply_discount
+      if self.discount != 0
+        self.total = self.total - (self.total*(self.discount.to_f/100)).round
+        "After the discount, the total comes to $#{self.total}."
+      else
+        "There is no discount to apply."
+      end
+    end
+
+    def void_last_transaction
+        self.items.pop
+        self.total -= self.transaction.pop
+    end
+
+end
